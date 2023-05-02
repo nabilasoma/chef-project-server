edbd283ba782a98c21f55261543cb1ab1c84f5ef
unknown <nabilasoma@gmail.com>
2023-05-02 16:21:53 +0600

    added

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const allChef = require('./data/allChef.json');
+const chefInfo = require('./data/chefInfo.json');
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.get('/allChef/:id', (req, res) => {
+    res.send(allChef)
+})
+
+app.get('/chefInfo', (req, res) => {
+    res.send(chefInfo)
+})
+
+app.get('/chefInfo/:id', (req, res) => {
+    const id = parseInt( req.params.id);
+    console.log(id);
+
+    const selectedInfo = chefInfo.find(info => parseInt(info.id) == id);
+    res.send(selectedInfo);
+})
+
+app.listen(port, () => {
+    console.log(`my server is running on port: ${port}`)
+})
